671af03a197dd3f9607007474915b6a7e25750b9
Deep Patel <pateldeep120404@gmail.com>
2025-08-09 17:46:21 +0530

    add home route in the backend

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
 app.use('/api/messages', messageRoutes);
 
 const server = http.createServer(app);
